267e41842535c47f006816332d3180277e21fa8b
Enrique Mascote <kike.mascote@gmail.com>
2019-06-14 12:18:46 -0600

    control

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemsComponent } from './components/pages/items/items.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ItemDetailComponent } from './components/pages/item-detail/item-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: ItemsComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'item-detail', component: ItemDetailComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { ItemsComponent } from './components/pages/items/items.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ItemDetailComponent } from './components/pages/item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ItemsComponent
+    ItemsComponent,
+    AboutComponent,
+    ItemDetailComponent
   ],
   imports: [
     BrowserModule,
